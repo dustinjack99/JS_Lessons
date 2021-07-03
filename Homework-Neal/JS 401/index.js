aa439ea285ff628ded15d6f1818b50ff25e9053b
JustCallMeNed <neal.c.fisher@gmail.com>
2021-07-02 22:13:12 -0500

    added WIP to 'Homework-Neal/JS_401'

--- Homework-Neal/JS 401/index.js
+++ Homework-Neal/JS 401/index.js
@@ -1,17 +1,10 @@
+// Base Classes
 class Hunter {
   constructor(outsideName) {
     this.hitPoints = 100;
-    this.attackPower = 5;
-    this.attackPool = outsideValue; //number of possible attacks per turn - shouldn't be any higher than 4
-    this.name = outsideName;
-  }
-}
-
-class Gunner {
-  constructor(outsideName) {
-    this.hitpoints = 100;
-    this.attackPower = 2;
-    this.attackPool = outsideValue; //number of possible attacks per turn - shouldn't be higher than 12
+    this.baseAttack = outsideValue; //Base attack power - damage per attack.
+    this.baseDefense = outsideValue; //Base Defense
+    this.attackPool = outsideValue; //number of possible attacks per turn
     this.name = outsideName;
   }
 }
@@ -19,10 +12,120 @@ class Gunner {
 class Devil {
   constructor(outsideName) {
     this.hitPoints = 100;
-    this.attackPower = 10;
-    this.attackPool = outsideValue; //number of possible attacks per turn - shouldn't be higher than 2
+    this.baseAttack = outsideValue;
+    this.baseDefense = outsideValue;
+    this.attackPool = outsideValue; //number of possible attacks per turn
     this.name = outsideName;
   }
 }
 
-//Fighter Classes
+//find attack power
+function findAttack(
+attackPower = this.baseAttack * this.attackPool)
+
+//Hunter Classes
+//findAttack should add up to 30 tops
+
+
+class Brawler extends Hunter { //Brawler - low attack, decent attack pool and defense
+    constructor() {
+      super();
+      this.baseAttack = 5
+      this.attackPool = 4
+      this.baseDefense = 20
+    }
+}
+
+class Tank extends Hunter { //Tank - high attack, high defense, low attack pool
+    constructor() {
+        super();
+        this.baseAttack = 30
+        this.attackPool = 1
+        this.baseDefense = 30
+    }
+}
+
+
+class Blaster extends Hunter { //Blaster - high attack, low defense
+    constructor() {
+        super();
+        this.baseAttack = 10
+        this.attackPool = 3
+        this.baseDefense = 5
+    }
+}
+//Gunner - low attack, high attack pool, low defense
+class Gunner extends Hunter {
+    constructor() {
+        super();
+        this.baseAttack = 2
+        this.attackPool = 12
+        this.baseDefense = 5
+    }
+}
+
+//Devil Classes
+
+class Toad extends Devil {
+    constructor() {
+        super();
+        this.baseAttack = 10
+        this.attackPool = 3
+        this.baseDefense = 30
+    }
+}
+
+class Manticore extends Devil {
+    constructor() {
+        super();
+        this.baseAttack = 5
+        this.attackPool = 10
+        this.baseDefense = 5
+    }
+}
+
+class Beelzebub extends Devil {
+    constructor() {
+        super();
+        this.baseAttack = 2
+        this.attackPool = 15
+        this.baseDefense = 1
+    }
+}
+
+//Basic Attack/Defense
+attack(target); {
+    findAttack();
+target.hitPoints = (target.hitPoints - this.attackPower);
+console.log(`${this.name} attacks ${target.name}!`);
+}
+//Basic melee attack. Tale as old as time.
+
+defend(target); {
+target.attackPower = (target.attackPower - this.baseDefense);
+console.log(`${this.name} puts up their guard against ${target.name}!`);
+}
+
+//Hunters
+const Osheen = new Brawler("Osheen Ferguson");
+this.name = Osheen
+console.log(Osheen)
+
+const Savannah = new Gunner("Savannah Smythe")
+this.name = Savannah
+console.log(Savannah)
+
+const Christopher = new Blaster("Christopher Laszaro");
+this.name = Christopher
+console.log(Christopher)
+
+const Columbina = new Tank("Columbina Laszaro");
+this.name = Columbina
+console.log(Columbina)
+
+//Devils
+const Toad = new Toad("Toad Demon")
+
+const Manticore = new Manticore("Manticore Demon")
+
+const Beelzebub = new Beelzebub("Bloatfly Demon")
