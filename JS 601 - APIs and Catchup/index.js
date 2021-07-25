88ca0b91ad755ef83d23371cdf8ccb32a020a4af
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-07-25 16:06:50 -0700

    notes for catch up lesson

--- JS 601 - APIs and Catchup/index.js
+++ JS 601 - APIs and Catchup/index.js
@@ -5,11 +5,98 @@
 
 // Topics:
 // 1. Variables, Data Types, Functions
+
+// variables - they are empty buckets that you can store data in
+// let timsVariable;
+
+// functions
+// they must be declared!
+// they must be called!
+
 // 2. Conditional Logic, Arrays, Loops
+
+// conditional logic
+// if / then statements
+// operators: >, <, ===, ||, &&
+// if(a > b || b < c) {
+// } else {
+// }
+
+// arrays
+// set of ordered data
+// a shelf of cubbies
+// how to instantiate an array:
+// let array = [1, 2, 3, 4];
+
+// how to access values
+// console.log(array[2]);
+// array.push();
+// array.forEach((item) => console.log(item + 1));
+// console.log(array);
+
 // 3. Git!
+// repository - community resevoir of code
+// common source
+// version control
+// prevents conflicts
+
+//git add .
+//git commit -m "little note"
+//git push origin <branch-name>
+
 // 4. DOM Manipulation / the DOM itself
+// DOCUMENT OBJECT MODEL
+// How elements relate to one another on the HTML document
+// Each element is called a node.
+
+// DOM manipulation methods
+// createElement('p');
+// appendChild();
+// textContent;
+
 // 5. Fetch and Async JS
+
+// fetch!
+// made up of request + response
+//
+
+// Async JS
+// When waiting for a response, a Promise is created
+// A response is what happens when the promise resolves
+// if the promise failed, you get an error response
+// if the promises succeeds, you can turn the data to JSON
+
+// JSON
+// JavaScript Object Notation
+// response.json()
+
 // 6. More about APIs
+// Application Program Interface
+// ferrying info from one PC to another
+// takes requests and serves info as requested
+// a set of rules that dictate what info is served based
+// what is requested
+// it is the parameters of the information that is requested
+
+//EXAMPLE OF API REQUESTS!
+// api.starwars.com/api/planets
+// hoth, tatooine, dantooine
+
+// api.starwars.com/api/sith
+// vader, dooku, maul
 
 // This week is going to be a cushion before we leap into...
 // Project #1!!!
+
+// MISC
+
+// common termonology:
+// METHOD - the name of a function already defined on an object.
+
+//               //parameters are in the declaration
+// function add (a, b) {
+//     return a + b;
+// }
+
+//     //arguments are in the call
+// add(2, 3);
