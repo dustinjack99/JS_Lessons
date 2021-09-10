020b8b1eb9ccd335473491928b035a44e9d23a1e
JustCallMeNed <neal.c.fisher@gmail.com>
2021-09-09 21:31:08 -0500

    Pushing up at D's request because he done fucked up.

--- Homework-Neal/Backend/NodeLesson2/server.js
+++ Homework-Neal/Backend/NodeLesson2/server.js
@@ -1,2 +1,39 @@
-// Use this file to write a server that routes to 3 index.html files from your previous homework assignments.
-
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const port = 8080;
+const displayHome = (res) => {
+fs.readFile(path.join(__dirname, "../../Frontend/JS 301 - DOM Manipulation"), (err, data) => {
+    if (err) {
+        console.log(err)
+    }else {
+        console.log(data)
+    // res.end(`
+    // <html>
+    //   <body>
+    //   <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
+    //   </body>
+    // </html>`)
+}
+const handleRequest = (req, res) => {
+  console.log(req.url);
+  if (req.url === "/home") {
+  }
+  res.end(`
+    <html>
+      <body>
+      <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
+      </body>
+    </html>`)
+} else if (req.url === "/"){
+    res.end(`
+      <html>
+        <body>
+        <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
+        </body>
+      </html>`);
+  } else {}
+const server = http.createServer(handleRequest);
+server.listen(port, () => {
+  console.log(`Server is running on port: ${port}.`);
+});
