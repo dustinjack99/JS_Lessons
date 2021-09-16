6c4f98a0d25f7df0eb2bee02333a19c3de12a5fa
JustCallMeNed <neal.c.fisher@gmail.com>
2021-09-16 17:57:01 -0500

    push up

--- Homework-Neal/Backend/NodeLesson2/server.js
+++ Homework-Neal/Backend/NodeLesson2/server.js
@@ -3,37 +3,54 @@ const fs = require("fs");
 const path = require("path");
 const port = 8080;
 const displayHome = (res) => {
-fs.readFile(path.join(__dirname, "../../Frontend/JS 301 - DOM Manipulation"), (err, data) => {
-    if (err) {
-        console.log(err)
-    }else {
-        console.log(data)
-    // res.end(`
-    // <html>
-    //   <body>
-    //   <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
-    //   </body>
-    // </html>`)
-}
-const handleRequest = (req, res) => {
-  console.log(req.url);
-  if (req.url === "/home") {
-  }
-  res.end(`
+  fs.readFile(
+    path.join(__dirname, "../../Frontend/JS 301 - DOM Manipulation"),
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+
+        res.end(`
     <html>
       <body>
       <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
       </body>
-    </html>`)
-} else if (req.url === "/"){
-    res.end(`
+    </html>`);
+      }
+      const displayError = (res) => {
+        res.end(`
+  <html>
+    <body>
+    <h1>Ha ha ha ha ha ha ha ha ha what a story Mark! Anyway how's your sex life.</h1>
+    </body>
+  </html>`);
+      };
+      const handleRequest = (req, res) => {
+        console.log(req.url);
+        if (req.url === "/home") {
+          res.end(`
+    <html>
+      <body>
+      <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
+      </body>
+    </html>`);
+        } else if (req.url === "/") {
+          res.end(`
       <html>
         <body>
         <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
         </body>
       </html>`);
-  } else {}
-const server = http.createServer(handleRequest);
-server.listen(port, () => {
-  console.log(`Server is running on port: ${port}.`);
-});
+        } else {
+        }
+        res.end(`
+<html>
+  <body>
+  <h1>Ha ha ha ha ha ha ha ha ha what a story Mark! Anyway how's your sex life.</h1>
+  </body>
+</html>`);
+      };
+    }
+  );
+};
--- Homework-Neal/Backend/NodeLesson2/server01.js
+++ Homework-Neal/Backend/NodeLesson2/server01.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const port = 8181;
+const port = 8080;
 const handleRequest = (req, res) => {
   res.end(
     `Some people say that if you stare at the moon for too long, you'll go mad. And I think that's true. Sir Patrick Moore has been staring at me for decades now and last night I watched him do a shit on the salad.`
