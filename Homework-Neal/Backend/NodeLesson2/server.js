619efc6ffcbc7570b61f5dcfbec6be1a8128c896
JustCallMeNed <neal.c.fisher@gmail.com>
2021-09-16 20:07:57 -0500

    Got homework done at the last second in a panic, thanks D XXXOOO

--- Homework-Neal/Backend/NodeLesson2/server.js
+++ Homework-Neal/Backend/NodeLesson2/server.js
@@ -2,55 +2,43 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const port = 8080;
+const displayError = (res) => {
+  res.end(`
+<html>
+<body>
+<h1>Ha ha ha ha ha ha ha ha ha what a story Mark! Anyway how's your sex life.</h1>
+</body>
+</html>`);
+};
+const handleRequest = (req, res) => {
+  console.log(req.url);
+  if (req.url === "/home") {
+    displayHome(res);
+  } else if (req.url === "/") {
+    displayHome(res);
+  } else {
+    displayError(res);
+  }
+};
 const displayHome = (res) => {
   fs.readFile(
-    path.join(__dirname, "../../Frontend/JS 301 - DOM Manipulation"),
+    path.join(__dirname, "../../Frontend/JS 301/index.html"),
     (err, data) => {
       if (err) {
         console.log(err);
       } else {
         console.log(data);
-
         res.end(`
     <html>
       <body>
-      <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
+      <h1>Ha ha ha ha ha ha ha what a story Mark. Anyway how's your sex life.</h1>
       </body>
     </html>`);
       }
-      const displayError = (res) => {
-        res.end(`
-  <html>
-    <body>
-    <h1>Ha ha ha ha ha ha ha ha ha what a story Mark! Anyway how's your sex life.</h1>
-    </body>
-  </html>`);
-      };
-      const handleRequest = (req, res) => {
-        console.log(req.url);
-        if (req.url === "/home") {
-          res.end(`
-    <html>
-      <body>
-      <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
-      </body>
-    </html>`);
-        } else if (req.url === "/") {
-          res.end(`
-      <html>
-        <body>
-        <h1>It's not true I did not hit her it's bullshit I did not. Oh hi Mark.</h1>
-        </body>
-      </html>`);
-        } else {
-        }
-        res.end(`
-<html>
-  <body>
-  <h1>Ha ha ha ha ha ha ha ha ha what a story Mark! Anyway how's your sex life.</h1>
-  </body>
-</html>`);
-      };
     }
   );
 };
+const server = http.createServer(handleRequest);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
