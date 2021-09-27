5c7dd3b2b9f9a39740de85ea00bc2bf95100bead
JustCallMeNed <neal.c.fisher@gmail.com>
2021-09-27 18:03:41 -0500

    Added to 'Homework-Neal/Backend/NodeLesson3/server.js

--- Homework-Neal/Backend/NodeLesson3/server.js
+++ Homework-Neal/Backend/NodeLesson3/server.js
@@ -1,1 +1,14 @@
-//No hints on this page! Feel free to reference the lesson, but DO NOT COPY / PASTE. I've ordered the lesson.js in such a way that this will do bad things.
+const db = require("./db.json");
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App is listening on port: ${port}`);
+});
