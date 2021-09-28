3bca88f462b11cde2e16906f0b82e1a7084b7efc
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-09-28 15:29:12 -0700

    last minute updates to sql lesson

--- Backend/2.3 - SQL/lesson.js
+++ Backend/2.3 - SQL/lesson.js
@@ -1,8 +1,14 @@
+// Go over CRUD Operations
+// Create, Read, Update, Delete.
+// MOVE TO EXAMPLE YAAAY
+// CREATE ("") -
+// READ ("") -
+// UPDATE ("") -
+// DELETE ("") -
+
+// SQL
 //used for "querying data"
 
-//SCHEMA - databases, think of them as a definition or the shape of the database. This is what our 'columns' should look like.  
+//SCHEMA - databases, think of them as a definition or the shape of the database. This is what our 'columns' should look like.
 
 //QUERY - bit of SQL language, how you interact with the database. SEMICOLONS MATTER.
-
-// Go over CRUD Verbs
-// Create, Read, Update, Delete.
