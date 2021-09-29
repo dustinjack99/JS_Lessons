1f0ad7d80c1f305461be4746826496099017897a
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-09-28 19:33:13 -0700

    SQL lesson done, notes available

--- Backend/2.3_SQL/lesson.js
+++ Backend/2.3_SQL/lesson.js
@@ -2,10 +2,10 @@
 // Create, Read, Update, Delete.
 // MOVE TO EXAMPLE YAAAY
 
-// CREATE ("") -
-// READ ("") -
-// UPDATE ("") -
-// DELETE ("") -
+// CREATE ("POST") - Creates an entry for the database.
+// READ ("GET") - Reads any info from the database.
+// UPDATE ("PUT") - Updates an entry in the database.
+// DELETE ("DELETE") - Removes an entry from the database.
 
 // SQL - structured querying language
 //used for "querying data"
