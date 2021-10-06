1e686eccd3eb81cc4ed461b3bf485d6c8312b6c9
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-10-05 19:55:04 -0700

    moresql lesson done, all students are experts in everything

--- Backend/2.4_MoreSQL/lesson.js
+++ Backend/2.4_MoreSQL/lesson.js
@@ -1,7 +1,94 @@
 //Connect to our database!
+// link docs for postgres, pg library, and inquirer library
+// also, stack overflow is your friend
 
-//make seed file
+const { Client } = require("pg");
 
-//BIG Data
-// go here: https://github.com/angrbrd/top5000-playlist/blob/master/TopSongs.csv
-// copy the contenst into a TopSongs.csv file
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "top_songs_db",
+  password: "Raptor99!",
+  port: 5432,
+});
+
+client.connect((err, res) => {
+  if (err) throw err;
+
+  console.log(
+    `Successfully connected to: ${res.database}. On Thread: ${res.processID}`
+  );
+
+  // all database functionality goes here!
+  //   selectAllArtists();
+  runSearch();
+});
+
+const selectAllArtists = async () => {
+  client.query("SELECT artist FROM top_5000;", (err, res) => {
+    if (err) throw err;
+
+    console.log(res.rows);
+    client.end();
+  });
+};
+
+const inquirer = require("inquirer");
+
+const runSearch = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to search from our Top 5000 songs?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who have more than one top song",
+        "Find all songs within a range",
+        "Find a specific song",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "Find songs by artist":
+          artistSearch();
+          break;
+        case "Find all artists who have more than one top song":
+          //   artistSearch();
+          break;
+        case "Find all songs within a range":
+          //   artistSearch();
+          break;
+        case "Find a specific song":
+          //   artistSearch();
+          break;
+        default:
+          client.end();
+      }
+    });
+};
+
+const artistSearch = () => {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What's the artist's name?",
+    })
+    .then((answer) => {
+      client.query(
+        "SELECT * FROM top_5000 WHERE artist = $1",
+        [answer.artist],
+        (err, res) => {
+          if (err) throw err;
+          console.log(res.rows);
+          client.end();
+        }
+      );
+    });
+};
+
+const findMultipleSongs = () => {};
+const findSongRange = () => {};
+const songSearch = () => {};
