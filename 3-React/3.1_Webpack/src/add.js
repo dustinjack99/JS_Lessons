f0622f9fa102518870a791af6f7fc5062eeb95fc
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-10-31 10:12:28 -0700

    webpack lesson done

---
+++ 3-React/3.1_Webpack/src/add.js
@@ -0,0 +1,5 @@
+export const addFuntcion = (a, b) => {
+  const sum = { sum: a + b };
+  const solution = { ...sum };
+  return solution;
+};
---
+++ 3-React/3.1_Webpack/src/app.js
@@ -0,0 +1,6 @@
+import { addFuntcion } from "./add";
+
+console.log("Hello, webpack!");
+console.log("woot");
+
+console.log(addFuntcion(4, 5));
