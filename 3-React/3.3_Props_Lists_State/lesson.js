86fff64b2a6ea7df545fbfc0ed28576da873928f
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-10-31 15:11:49 -0700

    create react app suuuuucks

---
+++ 3-React/3.3_Props_Lists_State/lesson.js
@@ -0,0 +1,12 @@
+// Components
+// Organized like a tree structure, much like the DOM.
+// Reusable, can consume data in the form of 'props'
+
+// JSX - JavaScript XML
+
+// show off state by making an increment / decrement counter.
+// class component
+// functional / hook component
+
+// Build a calculator app!
+// pair programming, Neal is going to share his screen
