e67a7aeab1feacbb62f606f85e206ad05d82ba19
JustCallMeNed <neal.c.fisher@gmail.com>
2021-12-05 22:51:04 -0600

    added folder 'dec-inc' for CodeAlong lesson

---
+++ Homework-Neal/3-React/3.3_Stateful_Components/dec-inc/src/App.js
@@ -0,0 +1,14 @@
+import IncDecClass from "./components/IncDecClass";
+import IncDecFunc from "./components/IncDecFunc";
+
+function App() {
+  return (
+    <>
+      <div>Bare Bones App</div>
+      {/* <IncDecClass /> */}
+      <IncDecFunc />
+    </>
+  );
+}
+
+export default App;
