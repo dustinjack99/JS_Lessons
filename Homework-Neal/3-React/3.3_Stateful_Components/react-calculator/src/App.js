b4153f8ad7f11533b3b0868e3b9dba9ee02b0d1b
JustCallMeNed <neal.c.fisher@gmail.com>
2021-12-06 23:02:01 -0600

    added App.css, starting to break up different classes of buttons/divs

--- Homework-Neal/3-React/3.3_Stateful_Components/react-calculator/src/App.js
+++ Homework-Neal/3-React/3.3_Stateful_Components/react-calculator/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/"];
+const buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const functions = ["+", "-", "*", "/"];
 
 function App() {
   const [display, setDisplay] = useState("");
@@ -9,34 +11,46 @@ function App() {
   return (
     <div className="App">
       <h1>Calculator App</h1>
-      {buttons.map((button) => (
-        <button onClick={() => setDisplay(display + button)}>{button}</button>
-      ))}
-      <button
-        onClick={() =>
-          setDisplay(
-            "OwO What's this? (notices ur " + eval(display.toString()) + ")"
-          )
-        }
-      >
-        Sexy
-      </button>
-      <button onClick={() => setDisplay("")}>Clear</button>
-      <button
-        onClick={() => {
-          if ((display = String)) {
+
+      <div id="display">
+        <h1>{display}</h1>
+      </div>
+      <hr />
+      <div className="Numbers">
+        {buttons.map((button) => (
+          <button onClick={() => setDisplay(display + button)}>{button}</button>
+        ))}
+      </div>
+      <div className="Functions">
+        {functions.map((button) => (
+          <button onClick={() => setDisplay(display + button)}>{button}</button>
+        ))}
+      </div>
+
+      <div className="Functions2">
+        <button
+          onClick={() =>
+            setDisplay(
+              "OwO What's this? (notices ur " + eval(display.toString()) + ")"
+            )
+          }
+        >
+          Sexy
+        </button>
+        <button onClick={() => setDisplay("")}>Clear</button>
+
+        <button
+          onClick={() => {
             setDisplay(eval(display.toString()));
             console.log(display);
+
             // has to run calculations on the display
             // 1. switch case
             // 2. eval()
-          }
-        }}
-      >
-        =
-      </button>
-      <div id="display">
-        <h1>{display}</h1>
+          }}
+        >
+          =
+        </button>
       </div>
     </div>
   );
