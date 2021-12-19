e9d5239c8dc9e3655f6ef6caf07d31baf3ddc39a
JustCallMeNed <neal.c.fisher@gmail.com>
2021-12-19 00:16:37 -0700

    broke css variables for font imports, but I swear they were working earlier. Also added buttons and animations.

--- Homework-Neal/3-React/3.3_Stateful_Components/react-calculator/src/App.js
+++ Homework-Neal/3-React/3.3_Stateful_Components/react-calculator/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const numerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const numerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0"];
 const functions = ["+", "-", "*", "/"];
 
 function App() {
