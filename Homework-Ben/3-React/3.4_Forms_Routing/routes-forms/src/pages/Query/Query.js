e8c61fa39bc6a97650e75fd3c28ecf85f1d59fbd
Val-code-1 <reit.ben@gmail.com>
2021-12-23 01:25:45 -0600

    Added a carousel for my titles that are returned, having trouble dry coding the carousel titles, need to add useEffect probably in SearchReturn if something new is searched

--- Homework-Ben/3-React/3.4_Forms_Routing/routes-forms/src/pages/Query/Query.js
+++ Homework-Ben/3-React/3.4_Forms_Routing/routes-forms/src/pages/Query/Query.js
@@ -1,15 +1,32 @@
 import { useState, useEffect } from "react";
 import SearchReturn from "./components/SearchReturn";
+import axios from "axios";
 
 const Query = () => {
-  const [search, setSearch] = useState(false);
   const [query, setQuery] = useState("");
+  const [apiJson, setApiJson] = useState(null);
+  const getJSON = async (query) => {
+    // const jsonData = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const jsonData = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${query}`);
+
+    setApiJson(jsonData.data);
+    console.log(jsonData.data);
+    console.log(apiJson);
+  };
   return (
     <div>
       <h1>Search the title of an Anime!</h1>
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}></input>
-      <button onClick={() => setSearch(true)}>Search</button>
-      {search ? <SearchReturn query={query} /> : <div></div>}
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setQuery(e.target.value);
+        }}
+      ></input>
+
+      <button onClick={() => getJSON(query)}>Search</button>
+      <SearchReturn apiJson={apiJson} />
     </div>
   );
 };
